fcd079267d6eb76f4dbae01073933ac0a3fa7c9f
enriquedev53 <enriquedev53@gmail.com>
2022-07-29 21:06:37 -0400

    feat(*): header created

---
+++ pages/nosotros.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+const nosotros = () => {
+  return (
+    <Layout 
+    pagina='Nosotros'>
+      <div>Desde Nosotros</div>
+    </Layout>
+  )
+}
+
+export default nosotros
---
+++ pages/tienda.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+const tienda = () => {
+  return (
+    <Layout 
+    pagina='Tienda'>
+      <div>Desde Tienda</div>
+    </Layout>
+  )
+}
+
+export default tienda
